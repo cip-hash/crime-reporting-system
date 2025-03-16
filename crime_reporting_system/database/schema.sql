fab29e53655c8ef2a41b7a4d51ca9aed1a2ecfd5
cip-hash <cipproject1234@gmail.com>
2025-03-16 15:00:34 +0530

    Update schema.sql

--- crime_reporting_system/database/schema.sql
+++ crime_reporting_system/database/schema.sql
@@ -3,10 +3,12 @@ CREATE DATABASE crime_reporting_system;
 
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(50) CHECK (role IN ('user', 'police', 'admin')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
