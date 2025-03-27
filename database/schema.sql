0d0f1d643fe4e6811c2d0bc832c61c58362da6eb
Santhosh <cipproject1234@gmail.com>
2025-03-27 15:31:19 +0530

    27/03

--- database/schema.sql
+++ database/schema.sql
@@ -1,59 +1,63 @@
-CREATE DATABASE register;
-
-USE register;
---MYSQL
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
---POSTGRES
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
-
- CREATE TABLE crime_reports (
-    id SERIAL PRIMARY KEY,
-     incident_type VARCHAR(50) NOT NULL,
-     date DATE NOT NULL,
-     time TIME NOT NULL,
-     district VARCHAR(100) NOT NULL,
-     subdivision VARCHAR(100) NOT NULL,
+--complaints table /*  nextval('complaints_id_seq'::regclass)  */
+CREATE TABLE complaints (
+    complaint_id VARCHAR(20) PRIMARY KEY,
+    incident_type VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time TIME WITHOUT TIME ZONE NOT NULL,
     description TEXT NOT NULL,
-    suspect TEXT,
-    victim TEXT,
-    witness TEXT,
-    evidence TEXT[], -- Array of file paths for evidence storage
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    suspect_details TEXT,
+    victim_details TEXT,
+    witness_details TEXT,
+    evidence_files JSONB,
+    status VARCHAR(50) DEFAULT 'Pending',
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    district VARCHAR(50) NOT NULL,
+    subdivision VARCHAR(50) NOT NULL,
+    complainant_email VARCHAR(100) NOT NULL,
+    CONSTRAINT fk_complaints_crime_stats FOREIGN KEY (district, subdivision) REFERENCES crime_statistics (district, subdivision)
 );
-select * from crime_reports;
+
+--crime_statistics
 CREATE TABLE crime_statistics (
+    district VARCHAR(100) NOT NULL,
+    subdivision VARCHAR(100) NOT NULL,
+    murder INTEGER,
+    murderforgain INTEGER,
+    dacoity INTEGER,
+    robbery INTEGER,
+    graveburglary INTEGER,
+    gravetheft INTEGER,
+    other INTEGER,
+    latitude NUMERIC(10,7),
+    longitude NUMERIC(10,7),
+    harassment INTEGER,
+    PRIMARY KEY (district, subdivision)
+);
+ALTER TABLE crime_statistics 
+ADD COLUMN totalcrimes INTEGER  GENERATED ALWAYS AS (
+    murder + murderforgain + dacoity + robbery + graveburglary + gravetheft + harassment+ other
+) STORED;
+
+
+--police
+CREATE TABLE police (
     id SERIAL PRIMARY KEY,
-    district TEXT,
-    subdivision TEXT,
-    murder INT,
-    murder_for_gain INT,
-    dacoity INT,
-    robbery INT,
-    grave_burglary INT,
-    grave_theft INT,
-    other INT,
-    total_crimes INT,
-    latitude DECIMAL(10, 6),
-    longitude DECIMAL(10, 6),
-        user_id INT
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    district VARCHAR(50) NOT NULL,
+    subdivision VARCHAR(50) NOT NULL
 );
 
 
-
-
-
+--user
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+);
